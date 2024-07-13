cf56ce5a867829c640af800658a3c5fda0f80d67
Christian Schmidt <47507599+chs2019@users.noreply.github.com>
2024-07-13 07:50:30 +0200

    fixt the test

--- tests/api.test.js
+++ tests/api.test.js
@@ -5,6 +5,6 @@ test("GitHub API returns the right response", async () => {
     "https://api.github.com/repos/mdn/content/stats/commit_activity",
   ).then((res) => res.json());
   // A year has 52 weeks
-  expect(data).toHaveLength(50);
+  expect(data).toHaveLength(52);
   expect(Object.keys(data[0])).toEqual(["total", "week", "days"]);
 });
